c2139366148a5079ca1ccede7609a6a90c201c03
Rihards Neimanis <rihards.neimanis@gmail.com>
2021-01-07 18:42:36 +0200

    Added support for external css and images

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   entry: {
@@ -21,14 +22,18 @@ module.exports = {
         test: /\.ts?$/,
         use: 'ts-loader',
         exclude: /node_modules/,
-      },
+      }
     ],
   },
   plugins: [
+    new CopyWebpackPlugin([
+      { from: 'src/styles/receiver.css', to: 'styles' },
+      { from: 'src/images', to: 'images' }
+    ]),
     new HtmlWebpackPlugin({
       hash: true,
       inject: 'head',
       template: './src/index.html',
-    }),
+    })
   ],
 };
